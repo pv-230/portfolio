97c29486b5c53516cc644063b703ecddf6aed493
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-03-16 13:04:26 -0400

    Add layout skeleton

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,43 @@
 import styled from 'styled-components';
 
 const StyledApp = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
   height: 100%;
 `;
 
 function App() {
-  return <StyledApp>Hello World</StyledApp>;
+  return (
+    <StyledApp>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+              <a href="/">Projects</a>
+              <a href="/">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <button type="button">Theme</button>
+      </header>
+      <main>
+        <section>
+          <h1>Home</h1>
+          <p>This is the home section.</p>
+        </section>
+        <section>
+          <h1>Projects</h1>
+          <p>This is the project section.</p>
+        </section>
+        <section>
+          <h1>Contact</h1>
+          <p>This is the contact section.</p>
+        </section>
+      </main>
+      <footer>
+        <p>This is the footer.</p>
+      </footer>
+    </StyledApp>
+  );
 }
 
 export default App;
