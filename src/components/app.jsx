6febe8ae10ed04ec6777de3405eb978c67a20580
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-03-16 13:46:07 -0400

    Reorganize minor project structure details

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,42 +1,54 @@
 import styled from 'styled-components';
 
+import GlobalStyle from '../common/global-styles';
+import '../common/reset.css';
+
 const StyledApp = styled.div`
   height: 100%;
 `;
 
+//-------------------------------------------------------------------------------------------------
+
 function App() {
   return (
-    <StyledApp>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <a href="/">Home</a>
-              <a href="/">Projects</a>
-              <a href="/">Contact</a>
-            </li>
-          </ul>
-        </nav>
-        <button type="button">Theme</button>
-      </header>
-      <main>
-        <section>
-          <h1>Home</h1>
-          <p>This is the home section.</p>
-        </section>
-        <section>
-          <h1>Projects</h1>
-          <p>This is the project section.</p>
-        </section>
-        <section>
-          <h1>Contact</h1>
-          <p>This is the contact section.</p>
-        </section>
-      </main>
-      <footer>
-        <p>This is the footer.</p>
-      </footer>
-    </StyledApp>
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href="/">About</a>
+                <a href="/">Projects</a>
+                <a href="/">Contact</a>
+              </li>
+            </ul>
+          </nav>
+          <button type="button">Theme</button>
+        </header>
+        <main>
+          <hgroup>
+            <h1>Peter Vasiljev</h1>
+            <p>Aspiring web developer</p>
+          </hgroup>
+          <section>
+            <h2>About</h2>
+            <p>This is the about section.</p>
+          </section>
+          <section>
+            <h2>Projects</h2>
+            <p>This is the project section.</p>
+          </section>
+          <section>
+            <h2>Contact</h2>
+            <p>This is the contact section.</p>
+          </section>
+        </main>
+        <footer>
+          <p>This is the footer.</p>
+        </footer>
+      </StyledApp>
+    </>
   );
 }
 
