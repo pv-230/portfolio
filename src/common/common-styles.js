deed27680eb7a24819e8990e1281b1e9c244cf4a
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-03-17 14:16:30 -0400

    Update section styling

--- src/common/common-styles.js
+++ src/common/common-styles.js
@@ -7,6 +7,8 @@ const StyledSection = styled.section`
   max-width: 60ch;
   min-height: 100vh;
   padding: 20px;
+  padding-top: calc(var(--header-height) + 20px);
+  gap: 20px;
 `;
 
 const SectionHeader = styled.h2`
