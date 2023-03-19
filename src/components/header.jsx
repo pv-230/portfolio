23c494b33e7fc96f2903919293a526207702a40e
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-03-19 15:26:13 -0400

    Remove color theme button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -27,27 +27,14 @@ const NavLink = styled.a`
   border-top: 5px solid transparent;
   border-bottom: 5px solid transparent;
 
-  &:focus {
+  &:focus-visible {
     outline: 1px solid var(--gold);
   }
 
-  &:focus,
+  &:focus-visible,
   &:hover {
     color: var(--gold);
     border-bottom: 5px solid var(--gold);
-  }
-`;
-
-const ThemeButton = styled.button`
-  width: 32px;
-  height: 32px;
-  background-color: transparent;
-  border: none;
-  padding: 0;
-  cursor: pointer;
-
-  &:focus {
-    outline: 1px solid var(--gold);
   }
 `;
 
@@ -61,14 +48,6 @@ function Header() {
         <NavLink href="#projects">Projects</NavLink>
         <NavLink href="#contact">Contact</NavLink>
       </NavList>
-      <ThemeButton type="button" title="Toggle theme">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true">
-          <path
-            fill="gold"
-            d="M12,2A7,7 0 0,0 5,9C5,11.38 6.19,13.47 8,14.74V17A1,1 0 0,0 9,18H15A1,1 0 0,0 16,17V14.74C17.81,13.47 19,11.38 19,9A7,7 0 0,0 12,2M9,21A1,1 0 0,0 10,22H14A1,1 0 0,0 15,21V20H9V21Z"
-          />
-        </svg>
-      </ThemeButton>
     </StyledHeader>
   );
 }
