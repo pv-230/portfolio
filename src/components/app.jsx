feaa5f838f020b7e7513c5e4018b14552a627af0
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-03-22 14:58:26 -0400

    Defer app rendering until fonts have loaded

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import '../common/reset.css';
@@ -14,23 +15,51 @@ const StyledApp = styled.div`
   background-color: var(--dark-10);
 `;
 
+const Blank = styled.div`
+  height: 100%;
+  background-color: var(--dark-10);
+`;
+
 //-------------------------------------------------------------------------------------------------
 
 function App() {
+  const [renderApp, setRenderApp] = useState(false);
+
+  /**
+   * Loads the primary font file and renders the app when loading has finished so that FOUC is
+   * is avoided. Fallback fonts will be used if an error is received during loading.
+   */
+  useEffect(() => {
+    const font = new FontFace(
+      'Source Sans Pro',
+      'url(https://fonts.gstatic.com/s/sourcesanspro/v21/6xK3dSBYKcSV-LCoeQqfX1RYOo3qNa7lqDY.woff2)'
+    );
+
+    document.fonts.add(font);
+    font.load();
+    document.fonts.ready
+      .catch((err) => console.error(`Unable to load 'Source Sans Pro' font: ${err.message}`))
+      .finally(() => setRenderApp(true));
+  }, []);
+
   return (
     <>
       <GlobalStyle />
-      <StyledApp>
-        <Header />
-        <main>
-          <About />
-          <Projects />
-          <Contact />
-        </main>
-        <footer>
-          <p>This is the footer.</p>
-        </footer>
-      </StyledApp>
+      {renderApp ? (
+        <StyledApp>
+          <Header />
+          <main>
+            <About />
+            <Projects />
+            <Contact />
+          </main>
+          <footer>
+            <p>This is the footer.</p>
+          </footer>
+        </StyledApp>
+      ) : (
+        <Blank />
+      )}
     </>
   );
 }
