aaa99d629e07a84689a674944c403f388d4bde4a
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-04-02 22:33:24 -0400

    Add info to project data

--- src/data/text-data.js
+++ src/data/text-data.js
@@ -1,12 +1,69 @@
 const projects = [
   {
+    name: "Where's Waldo?",
+    description:
+      'How fast can you find Waldo and the others? Choose between three scenes and submit your ' +
+      'time to the leaderboard.',
+    tech: ['React', 'React Router', 'Firebase', 'Styled Components'],
+    liveLink: 'https://pv-230.github.io/odin-wheres-waldo/',
+    githubLink: 'https://github.com/pv-230/odin-wheres-waldo',
+  },
+  {
+    name: 'Battleship',
+    description:
+      "A recreation of Milton Bradley's classic board game. Play against a computer and try to" +
+      'sink all their ships. Primarily an exercise in using jest for unit tests, however the ' +
+      'logic for the opponent became somewhat of a focus.',
+    tech: ['Jest', 'Webpack'],
+    liveLink: 'https://pv-230.github.io/odin-battleship/',
+    githubLink: 'https://github.com/pv-230/odin-battleship',
+  },
+  {
+    name: 'Memory Card Game',
+    description:
+      'Try to click on each card only once. The deck shuffles after each click. This ' +
+      'project was created to practing using React components with hooks.',
+    tech: ['React', 'Webpack'],
+    liveLink: 'https://pv-230.github.io/odin-memory-card/',
+    githubLink: 'https://github.com/pv-230/odin-memory-card',
+  },
+  {
+    name: 'Simple Shopping Website',
+    description:
+      'Shop for fruits and manage your cart. This project was created to practice ' +
+      'using Jest with React Testing Library and to also practice creating a single page app with ' +
+      'React Router.',
+    tech: ['React', 'Jest', 'React Testing Library', 'React Router'],
+    liveLink: 'https://pv-230.github.io/odin-shopping-cart/',
+    githubLink: 'https://github.com/pv-230/odin-shopping-cart',
+  },
+  {
+    name: 'Canvas 2',
+    description:
+      'Group term project that tries to recreate the learning management system known by ' +
+      'students and teachers as Canvas. Visit the GitHub link for more details.',
+    tech: ['Flask', 'MongoDB', 'Docker', 'Pytest'],
+    liveLink: null,
+    githubLink: 'https://github.com/pv-230/canvas2-web-app',
+  },
+  {
+    name: 'CS Semester Planner',
+    description:
+      'Group term project that suggests various ways to register for classes each ' +
+      "semester based on a student's completed prerequisites and the degree requirements.",
+    tech: ['React', 'Jest', 'React Testing Library', 'React Router', 'Firebase'],
+    liveLink: null,
+    githubLink: 'https://github.com/pv-230/se-capstone-web-app',
+  },
+  /*
+  {
     name: '',
     description: '',
     tech: [''],
-    previewSrc: '',
     liveLink: '',
     githubLink: '',
   },
+  */
 ];
 
 export default projects;
