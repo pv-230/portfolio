d5750e672442fdbd36d3666648a0c9089e0d7981
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-04-15 17:55:17 -0400

    Adjust contact section structure

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,16 +3,22 @@ import styled from 'styled-components';
 
 import { StyledSection, SectionHeader } from '../common/common-styles';
 
-const StyledContact = styled(StyledSection)`
-  color: var(--light-font-color);
+const StyledContact = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  background-color: var(--dark-bg-color);
 `;
 
 //-------------------------------------------------------------------------------------------------
 
 const Contact = forwardRef((props, ref) => (
   <StyledContact id="contact" ref={ref}>
-    <SectionHeader>Contact</SectionHeader>
-    <p>This is the contact section.</p>
+    <StyledSection>
+      <SectionHeader>Contact</SectionHeader>
+      <p>This is the contact section.</p>
+    </StyledSection>
   </StyledContact>
 ));
 
