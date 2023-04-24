b16597ec17eed95d889e9d880fdca40978d7fd0d
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-04-24 18:50:40 -0400

    Fix missing contact component return

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,12 +3,14 @@ import { SectionContainer, StyledSection } from '../common/common-styles';
 //-------------------------------------------------------------------------------------------------
 
 function Contact() {
-  <SectionContainer>
-    <StyledSection>
-      <h2>Contact</h2>
-      <p>This is the contact section.</p>
-    </StyledSection>
-  </SectionContainer>;
+  return (
+    <SectionContainer>
+      <StyledSection>
+        <h2>Contact</h2>
+        <p>This is the contact section.</p>
+      </StyledSection>
+    </SectionContainer>
+  );
 }
 
 export default Contact;
