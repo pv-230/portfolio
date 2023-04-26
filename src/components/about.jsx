8b6dac45673aee8b25c5d6f22c66183e2c5b03f0
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-04-25 20:01:30 -0400

    Remove forward ref from About component

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,4 +1,3 @@
-import { forwardRef } from 'react';
 import styled from 'styled-components';
 
 import {
@@ -16,29 +15,37 @@ const StrongText = styled.strong`
 
 //-------------------------------------------------------------------------------------------------
 
-const About = forwardRef((props, ref) => (
-  <SectionContainer id="about" ref={ref}>
-    <StyledSection>
-      <SectionHeader>About</SectionHeader>
-      <ParagraphContainer>
-        <StyledParagraph>
-          My goal is to become a full-time web developer. My earliest experience with computers was
-          in the 90&apos;s as a kid playing DOS games. Eventually, I decided to pursue a degree in
-          computer science. My interest in web development began during my final semesters where I
-          worked with a couple groups to build full-stack web applications.
-        </StyledParagraph>
-        <StyledParagraph>
-          My focus is on front-end web development, although I have some experience with{' '}
-          <StrongText>Flask</StrongText>, <StrongText>MongoDB</StrongText>,{' '}
-          <StrongText>Firebase</StrongText>, and <StrongText> MySQL</StrongText>. There is so much
-          to learn about web development and I find myself experimenting with different ways to
-          build projects. I currently prefer to build my projects using{' '}
-          <StrongText>React</StrongText>, <StrongText>Vite</StrongText>, and{' '}
-          <StrongText>Styled Components</StrongText>.
-        </StyledParagraph>
-      </ParagraphContainer>
-    </StyledSection>
-  </SectionContainer>
-));
+function About() {
+  return (
+    <SectionContainer>
+      <StyledSection>
+        <SectionHeader>About</SectionHeader>
+        <ParagraphContainer>
+          <StyledParagraph>
+            My goal is to become a full-time web developer. My earliest experience with computers
+            was in the 90&apos;s as a kid playing DOS games. Eventually, I decided to pursue a
+            degree in computer science. My interest in web development began during my final
+            semesters where I worked with a couple groups to build full-stack web applications.
+          </StyledParagraph>
+          <StyledParagraph>
+            My focus is on front-end web development, although I have some experience with{' '}
+            <StrongText>Flask</StrongText>, <StrongText>MongoDB</StrongText>,{' '}
+            <StrongText>Firebase</StrongText>, and <StrongText> MySQL</StrongText>. There is so
+            much to learn about web development and I find myself experimenting with different ways
+            to build projects. I currently prefer to build my projects using{' '}
+            <StrongText>React</StrongText>, <StrongText>Vite</StrongText>, and{' '}
+            <StrongText>Styled Components</StrongText>.
+          </StyledParagraph>
+        </ParagraphContainer>
+        <ParagraphContainer>
+          <hgroup>
+            <h3>Skills</h3>
+            <p>Here are some things I have worked with before</p>
+          </hgroup>
+        </ParagraphContainer>
+      </StyledSection>
+    </SectionContainer>
+  );
+}
 
 export default About;
