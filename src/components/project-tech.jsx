4d4be715a041dfcd90f77a237e662ef95f7a4284
pv-230 <81388791+pv-230@users.noreply.github.com>
2023-05-20 22:20:15 -0400

    Refactor project tech list to use list element

--- src/components/project-tech.jsx
+++ src/components/project-tech.jsx
@@ -3,10 +3,15 @@ import PropTypes from 'prop-types';
 
 import { TechIcon } from '../common/common-styles';
 
-const TechList = styled.div`
+const TechList = styled.ul`
   display: flex;
   gap: 10px;
   padding: 20px 0;
+  margin: 0;
+`;
+
+const TechListItem = styled.li`
+  list-style: none;
 `;
 
 //-------------------------------------------------------------------------------------------------
@@ -17,13 +22,14 @@ function ProjectTech({ techArr }) {
       {techArr.map((tech, i) => {
         const filename = tech.toLowerCase().replaceAll(' ', '-');
         return (
-          <TechIcon
-            src={new URL(`/src/assets/svg/${filename}.svg`, import.meta.url).href}
-            alt={techArr[i]}
-            title={techArr[i]}
-            // eslint-disable-next-line react/no-array-index-key
-            key={i}
-          />
+          // eslint-disable-next-line react/no-array-index-key
+          <TechListItem key={i}>
+            <TechIcon
+              src={new URL(`/src/assets/svg/${filename}.svg`, import.meta.url).href}
+              alt={techArr[i]}
+              title={techArr[i]}
+            />
+          </TechListItem>
         );
       })}
     </TechList>
